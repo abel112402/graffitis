9f64315c665fd92982733c9038c7f6f30d7bde0b
Ábel Tamás Burián <dmkseh@INF.ELTE.HU>
2025-10-19 13:51:11 +0200

    basic_setup

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const SUPABASE_URL = 'https://<PROJECT_REF>.supabase.co'
+const SUPABASE_ANON_KEY = '<ANON_PUBLIC_KEY>'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
